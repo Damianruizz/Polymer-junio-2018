d077bea35586c4c9827c2385c8f01773784e4f22
Damian <damian.ruiz@outlook.es>
2018-06-05 15:43:34 -0500

    js changes

---
+++ anuarHarb/layouts/index.js
@@ -0,0 +1,12 @@
+(() => {
+	'use strict';
+
+	const sidenavButton = document.getElementsByClassName('toggleSidenav');
+	const sideNav = document.querySelector('.sidenav');
+
+	for(let button of sidenavButton){
+		button.addEventListener('click', () => {
+			sideNav.classList.add('sidenav--open');
+		});
+	}
+})();
