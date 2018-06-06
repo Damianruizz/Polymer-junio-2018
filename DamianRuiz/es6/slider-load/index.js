920518316227a2d89ba17ff4d45d6b8b93a74e3a
Damian <damian.ruiz@outlook.es>
2018-06-06 17:30:28 -0500

    create slider load exercise

---
+++ DamianRuiz/es6/slider-load/index.js
@@ -0,0 +1,25 @@
+(() => {
+
+	let button = document.getElementById('initCharge');
+
+	button.addEventListener('click', (e) => {
+		let advance = 0;
+		document.getElementById("messagge").innerHTML = '';
+		let slider = document.getElementById('slider');
+
+		let interval = setInterval(() => { 
+			if (advance <= 100) {
+				advance += getAdvance();
+				slider.style.width = `${advance}%`;
+			} else {
+				clearInterval(interval);
+				document.getElementById("messagge").innerHTML = '¡Carga Exitosa!';
+			} 
+		}, 200);
+	});
+
+	function getAdvance(){
+		return Math.floor((Math.random() * 10) + 1);
+	}
+
+})();
