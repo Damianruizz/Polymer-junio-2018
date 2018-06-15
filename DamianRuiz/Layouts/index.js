f53911c9b2da1bfff478b980bc9dce78b76705c5
robertodamian.ruiz.contractor <damian.ruiz@outlook.es>
2018-06-15 10:59:49 -0500

    add responsive mobile to layouts exercise

--- DamianRuiz/Layouts/index.js
+++ DamianRuiz/Layouts/index.js
@@ -1,3 +1,13 @@
-function toggleHamburgerButton(x) {
-    x.classList.toggle("change");
-}
+(function() {
+  'use strict';
+
+  const sidenavButton = document.getElementsByClassName('toggleSidenav');
+  const sideNav = document.querySelector('.sidenav');
+
+  for(let button of sidenavButton) {
+    button.addEventListener('click', () => {
+      sideNav.classList.toggle('sidenav--open');
+    });
+  };
+
+})();
